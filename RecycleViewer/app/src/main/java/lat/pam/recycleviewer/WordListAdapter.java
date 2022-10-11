c96ac18af863d157d5cffe19c5509fe602210365
user <jalantikus360@gmail.com>
2022-10-11 21:52:03 +0700

    commit tugas

---
+++ RecycleViewer/app/src/main/java/lat/pam/recycleviewer/WordListAdapter.java
@@ -0,0 +1,71 @@
+package lat.pam.recycleviewer;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate an item view.
+        View mItemView = mInflater.inflate(
+                R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+    public int getItemCount() {
+        return mWordList.size();
+    }
+    public WordListAdapter(FirstFragment context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context.getContext());
+        this.mWordList = wordList;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    public class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        public WordViewHolder(@NonNull View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+
+
+        }
+        @SuppressLint("NotifyDataSetChanged")
+        @Override
+        public void onClick(View v) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            // Use that to access the affected item in mWordList.
+            String element = mWordList.get(mPosition);
+            // Change the word in the mWordList.
+            mWordList.set(mPosition, "Clicked! " + element);
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged();
+
+        }
+
+
+    }
+    }
+
